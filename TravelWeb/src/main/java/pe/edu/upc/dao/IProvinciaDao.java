42e9b45f3d1c5cff1d8f91a78c22daed15a6b0cb
CarlosCR2307 <u201816727@upc.edu.pe>
2021-05-02 10:50:17 -0500

    Provincia DAO

--- TravelWeb/src/main/java/pe/edu/upc/dao/IProvinciaDao.java
+++ TravelWeb/src/main/java/pe/edu/upc/dao/IProvinciaDao.java
@@ -3,11 +3,12 @@ package pe.edu.upc.dao;
 
 import java.util.List;
 
+
 import pe.edu.upc.entity.Provincia;
 
 public interface IProvinciaDao {
 	public void insertar(Provincia provincia);
 	public List<Provincia> listar();
 	public void eliminar(int idProvincia);
-	
+	public void actualizar(Provincia provincia);
 }
